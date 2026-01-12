e8f75da6ca16f2cf000bd0db5f89f0c81f1e5bbb
Distiere <bugman3652@gmail.com>
2026-01-12 10:02:11 +0100

    risoluzione problema analisi colonne per procedere definitivamente con mock up silver e gold

---
+++ explore_Schema.py
@@ -0,0 +1,36 @@
+import polars as pl
+
+calls_path = "data/raw/Fire_Department_Calls_for_Service.csv"
+
+lf = pl.scan_csv(
+    calls_path,
+    infer_schema=False,
+)
+
+cols = lf.collect_schema().names()
+
+print(f"Numero colonne: {len(cols)}\n")
+for c in cols:
+    print(c)
+
+with open("columns_calls_CSV_1.txt", "w") as f:
+    for c in lf.columns:
+        f.write(c + "\n")
+
+calls_path_2 = "data/raw/Fire_Incidents.csv"
+
+lf_2 = pl.scan_csv(
+    calls_path_2,
+    infer_schema=False,
+)
+
+cols = lf_2.collect_schema().names()
+
+print(f"Numero colonne: {len(cols)}\n")
+for c in cols:
+    print(c)
+
+with open("columns_calls_CSV_2.txt", "w") as f:
+    for c in lf.columns:
+        f.write(c + "\n")
+
